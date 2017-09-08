a9b6a922351d13ac5d2581ddf687e6cf51063e34
CWINET\tefferson.guterres <tefferson.guterres@cwi.com.br>
2017-09-08 14:42:13 -0300

    style(css): add css

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,26 @@ var gulp = require('gulp');
 var stylus = require('gulp-stylus');
 var autoprefixer = require('gulp-autoprefixer');
 
+function BuildConfiguration(filePaths, folderDist, compiledFileName) {
+    this.src = filePaths;
+    this.dist = folderDist;
+    this.fileName = compiledFileName;
+    this.completePath = function () {
+        return this.dist + this.fileName;
+    };
+}
+
 gulp.task('style', function () {
-  return gulp.src('stylus/app.styl')
+  return gulp.src('stylus/site.styl')
     .pipe(stylus())
     .pipe(autoprefixer())
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('css'));
+});
+
+gulp.task('watch', function () {
+	var config = new BuildConfiguration(['stylus/*.styl'], 'css', 'site.css');
+    gulp.watch(config.src, ['style']);
 });
 
 gulp.task('default', ['style']);
+gulp.run('watch');
