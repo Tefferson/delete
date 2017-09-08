5a9ef2564df6240b21dd4065c5af8ed31eed6698
CWINET\tefferson.guterres <tefferson.guterres@cwi.com.br>
2017-09-08 16:03:56 -0300

    chore(version): add random version to scripts with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var stylus = require('gulp-stylus');
 var autoprefixer = require('gulp-autoprefixer');
+var version = require('gulp-version-number');
 
 function BuildConfiguration(filePaths, folderDist, compiledFileName) {
     this.src = filePaths;
@@ -11,6 +12,15 @@ function BuildConfiguration(filePaths, folderDist, compiledFileName) {
     };
 }
 
+var path = '*.html';
+var config = {
+    'replaces':
+        [
+            [/\.js(\?\d+)?/g, '.js?' + Math.floor(Math.random() * 1000)],
+            [/\.css(?!\()(\?\d+)?/g, '.css?' + Math.floor(Math.random() * 1000)]
+        ]
+};
+
 gulp.task('style', function () {
   return gulp.src('stylus/site.styl')
     .pipe(stylus())
@@ -18,10 +28,16 @@ gulp.task('style', function () {
     .pipe(gulp.dest('css'));
 });
 
+gulp.task('version-number', function () {
+  return gulp.src(path, { base: '' })            
+    .pipe(version(config))
+    .pipe(gulp.dest(''));
+});
+
 gulp.task('watch', function () {
 	var config = new BuildConfiguration(['stylus/*.styl'], 'css', 'site.css');
     gulp.watch(config.src, ['style']);
 });
 
-gulp.task('default', ['style']);
+gulp.task('default', ['version-number', 'style']);
 gulp.run('watch');
