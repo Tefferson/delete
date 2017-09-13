aafd08a19999375635c4e774cdbe9f315d93e82a
CWINET\tefferson.guterres <tefferson.guterres@cwi.com.br>
2017-09-13 16:31:54 -0300

    no message

---
+++ dist/module.js
@@ -0,0 +1,1 @@
+var app=angular.module("TeffersonApp",["ngRoute"]);app.config(function($routeProvider){$routeProvider.when("/test",{templateUrl:"app/components/test/test.html",controller:"testController"}).when("/main",{templateUrl:"app/components/main/main.html",controller:"mainController"}).otherwise({redirectTo:"/main"})}),app.run(function(){});
